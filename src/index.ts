33b1c2b941f931be9bd2a50b242eff8cf56aad14
GastonFIP <104107514+GastonFIP@users.noreply.github.com>
2022-04-27 18:54:00 -0300

    Agremos un nuevo comentario Comentario durante la clase

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ console.log(
   "Este es nuestro primer commit, y me salio bien. No se que va a pasar ahora"
 );
 console.log("agregue mas datos esta segunda clase");
+console.log("este comentario se agrego en vivo durante la clase");
